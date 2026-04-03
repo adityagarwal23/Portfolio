f5d6fb054b11df28832d6469918472b58e172e94
Aditya Agarwal <101117030+adityagarwal23@users.noreply.github.com>
2026-04-03 15:45:20 -0400

    update proj

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -31,14 +31,12 @@ export const Projects = () => {
             </div>
 
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2">Comic List Processor</h3>
+              <h3 className="text-xl font-bold mb-2">Restaurant Menu</h3>
               <p className="text-gray-400 mb-4">
-                High-performance C application for managing a comic database.
-                Includes functionality for searching, adding, and deleting entries,
-                optimized for large datasets.
+                Developed a native Android restaurant menu application in Kotlin to digitize menus for small restaurants.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["C"].map((tech, key) => (
+                {["Kotlin", "Java"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 transition-all hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)]"
